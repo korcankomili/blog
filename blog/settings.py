a58316f882c061210e4c6e6e9cb9e387cb8aaeb9
korcankomili <korcankomili@gmail.com>
2020-06-20 15:38:25 +0300

    models were created

--- blog/settings.py
+++ blog/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +66,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'blogdb.sqlite3'),
     }
 }
 
