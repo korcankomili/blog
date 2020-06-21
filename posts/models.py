39bdea181405697648e29678cc62d6d1e2486974
korcankomili <korcankomili@gmail.com>
2020-06-21 04:12:17 +0300

    categories widget

--- posts/models.py
+++ posts/models.py
@@ -34,10 +34,7 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={
             'id': self.id
-        }) 
-
-
-
+        })  
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,18 @@
+from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
 from .models import Post,Author
 from marketing.models import Signup
+
+
+def get_category_count():
+    queryset = Post \
+            .objects \
+            .values('categories__title') \
+            .annotate(Count('categories__title'))
+        
+    return queryset
+
 
 def index(request):
     
@@ -22,8 +33,10 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+
+    category_count = get_category_count() 
     post_list = Post.objects.all()
-    paginator = Paginator(post_list,3)
+    paginator = Paginator(post_list,4)
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
 
@@ -39,7 +52,8 @@ def blog(request):
     context = {
         'queryset': paginated_queryset,
         'most_recent':most_recent,
-        'page_request_var':page_request_var
+        'page_request_var':page_request_var,
+        'category_count':category_count
     } 
 
     return render(request, 'blog.html', context)
